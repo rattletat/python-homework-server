ea48df56c0e0e6a9faf7477e748f745921a983c3
Michael Brauweiler <michael.brauweiler@posteo.de>
2020-05-12 09:13:14 +0200

    Beautify error feedback

--- exercises/templatetags/stacktrace.py
+++ exercises/templatetags/stacktrace.py
@@ -11,13 +11,18 @@ def beautify_error(error):
     lines = error.split("\n")
     error_index = [i for i, line in enumerate(lines) if "Error" in line][0]
     error_line = lines[error_index]
-    question_line = lines[error_index-1]
+    question_line = lines[error_index - 1]
 
     try:
         if "AssertionError" in error_line:
-            mq = re.search(r'self\.assert.+\(submission\.([^(]+\(.+\)), ".*"\)$', question_line)
+            mq = re.search(
+                r'self\.assert.+\(submission\.([^(]+\(.+\)), ".*"\)$', question_line
+            )
             me = re.search("'([^']*)' != '([^']*)'", error_line)
-            return f"Aufruf: {mq.group(1)}\nFalsch: {me.group(1)}\nLösung: {me.group(2)}"
+            return (
+                f"Aufruf: {mq.group(1)}\nFalsch: {me.group(1)}\nLösung: {me.group(2)}"
+            )
     except AttributeError:
         pass
-    return f"{question_line}\n{error_line}"
+    lines = "\n".join(lines[error_index - 2:])
+    return f"{lines}"
