3b3a243115421af80265c37b952a82d63f306590
Michael Brauweiler <michael.brauweiler@posteo.de>
2020-05-13 20:48:32 +0200

    Set maximum limit on test statistics

--- exercises/queries.py
+++ exercises/queries.py
@@ -1,6 +1,4 @@
 from exercises.models import TestResult
-from django.db.models import FloatField, F
-from django.db.models.functions import Cast
 
 
 def get_user_test_results(user, exercise):
@@ -12,15 +10,19 @@ def get_user_test_results(user, exercise):
 def get_user_test_statistics(user, exercise):
     tests = get_user_test_results(user, exercise)
     if tests:
-        return (
+        result = (
             tests.exclude(test_count=0)
-            .annotate(
-                success_rate=Cast(F("success_count") * 100, FloatField())
-                / Cast(F("test_count"), FloatField())
-            )
-            .values("success_rate", "test_count", "success_count")
+            .values("test_count", "success_count")
             .order_by("-success_count")[0]
         )
+        # Set upper limit on maximal tests
+        # (needed when amount of tests is changed afterwards)
+        result["test_count"] = min(result["test_count"], exercise.max_tests)
+        result["success_count"] = min(
+            result["success_count"], exercise.max_tests
+        )
+        result["success_rate"] = result["success_count"] / result["test_count"]
+        return result
     else:
         return {
             "success_rate": 0,
