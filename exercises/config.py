eeeb88f6f9b638282dca22a07dde015516679a72
Michael Brauweiler <michael.brauweiler@posteo.de>
2020-06-14 22:23:57 +0200

    Enable write access

--- exercises/config.py
+++ exercises/config.py
@@ -2,17 +2,18 @@ import os
 
 # === DOCKER OPTIONS ===
 
-DOCKER_IMAGE = "python:3"
+DOCKER_IMAGE = "test-container"
 DOCKER_WORKING_DIR = "/app"
 DOCKER_SETUP_OPTIONS = {
     "working_dir": DOCKER_WORKING_DIR,
 }
 DOCKER_SECURITY_OPTIONS = {
     "user": "1000:1000",
-    "read_only": True,
+    # "read_only": True,
     "network_disabled": True,
     "network_mode": "none",
     "mem_limit": "1g",
     "cap_drop": ["ALL"],
+    "privileged": False,
 }
 DOCKER_RUNNER_PATH = os.path.join("/exercises", "utils", "runner.py")
