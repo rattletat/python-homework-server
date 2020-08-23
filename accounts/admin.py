c3c9e6775a511e331a34c6c870fe75574bb129ba
Michael Brauweiler <michael.brauweiler@posteo.de>
2020-08-23 19:24:17 +0200

    Add scoreboard to admin user list

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from accounts.models import User
-from exercises.models import Submission
+from exercises.models import Submission, Exercise
+from exercises.queries import get_user_test_statistics, convert_points_to_grade
 
 
 class SubmissionInline(admin.StackedInline):
@@ -12,12 +13,21 @@ class SubmissionInline(admin.StackedInline):
 
 
 class UserAdmin(admin.ModelAdmin):
-    fields = ("last_login", "email", "identifier")
-    list_display = ("email", "last_login")
+    fields = ("last_login", "email", "identifier", "points", "grade")
+    list_display = ("email", "identifier", "points", "grade")
     readonly_fields = ["last_login"]
     inlines = [
         SubmissionInline,
     ]
+
+    def points(self, obj):
+        return sum(
+            get_user_test_statistics(obj, exercise)['success_count']
+            for exercise in Exercise.objects.all()
+        )
+
+    def grade(self, obj):
+        return convert_points_to_grade(self.points(obj))
 
 
 admin.site.register(User, UserAdmin)
